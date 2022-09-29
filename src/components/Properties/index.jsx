74bffccb3ce3f1123c98bcbb376391626cc64b8b
alideveloper <alideveloper09@gmail.com>
2022-09-29 00:23:11 -0500

    bugs fixed

--- src/components/Properties/index.jsx
+++ src/components/Properties/index.jsx
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {Container} from "./style";
 import HouseCard from "../HouseCard";
 import {useLocation} from "react-router-dom";
-const {REACT_APP_BASE_URL: url} = process.env
+// const {REACT_APP_BASE_URL: url} = process.env
+const url = 'https://houzing-app.herokuapp.com/api/v1'
 
 const Properties = () => {
     const [data, setData] = useState([])
@@ -12,7 +13,6 @@ const Properties = () => {
             .then(res => res.json())
             .then(res => setData(res?.data || []))
     }, [search])
-    console.log(data)
     return (
         <Container>
             {data.map(value => {
